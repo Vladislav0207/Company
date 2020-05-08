28470b002be14a4074e0f3c9258fa2d0dc5aacee
Vladislav <37775244+Vinny94051@users.noreply.github.com>
2020-05-08 14:01:11 +0300

    Review

--- company_Plane.java
+++ company_Plane.java
@@ -3,14 +3,15 @@ package com.company;
 public class Plane {
     String mark;
     int places;
-    int mass;
+    int mass; //weight
 
     public Plane(String mark, int places, int mass) {
         this.mark = mark;
         this.places = places;
-        this.mass=mass;
+        this.mass = mass;
     }
 
+    //То же самое, toString()
     void info() {
         System.out.println("марка " + mark + "\nместа " + places+"\nмасса "+mass);
     }
